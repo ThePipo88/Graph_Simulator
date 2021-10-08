57782b919fac165282c29d07bb94d7cab4534a9b
Reyner Badilla Arias <reyner.badilla.arias@est.una.ac.cr>
2021-10-08 11:44:55 -0600

    Rotar linea

--- Proyecto_2/Proyecto_2/Proyecto_2.cpp
+++ Proyecto_2/Proyecto_2/Proyecto_2.cpp
@@ -1,8 +1,30 @@
-#include <iostream>
+
+#include <SFML/Graphics.hpp>
+
 using namespace std;
+using namespace sf;
 
 int main()
 {
-    cout << "Hello World" << endl;
-}
+    RenderWindow window(VideoMode(700, 700), "SFML works!");
+    RectangleShape line(sf::Vector2f(150, 5));
+    line.rotate(70);
+    line.setPosition(Vector2f(150, 150));
+    line.setFillColor(Color::Green);
 
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(line);
+        window.display();
+    }
+
+    return 0;
+}
